74654552b8677beb83122cf87c62c5f1c32ed733
usmanzaib <usmanzaib356@gmail.com>
2023-07-13 04:29:01 -0700

    initial

--- src/Components/Subcribe.jsx
+++ src/Components/Subcribe.jsx
@@ -6,10 +6,10 @@ function Subcribe() {
     <div className='w-full px-4 py-16 bg-black text-center text-white'>
          <h1 className='fz'> Join our DeFi Community  </h1>
          <div className='py-4'>
-            <input className='sm:p-3 inp rounded-3xl mr-4 text-black' type="email" placeholder='Input your email'  />
+            <input className='sm:p-3 inp rounded-3xl mr-4 text-black text-center' type="email" placeholder='Input your email'  />
             <button className='btnnn'> Sign Up </button>
          </div>
-         <div className='flex justify-center  py-2 gap-3  '>
+         <div className='flex justify-center  py-2 gap-1  '>
              <input  type='checkbox' className='check' />
              <p className='fzp'> Yes, I agree to receive email communications from Defi. </p>
          </div>
